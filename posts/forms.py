c7ab70db459bf6a584ea9911317c3b246f886577
Stephniechi30 <stephniechi@gmail.com>
2022-03-24 12:17:28 +0100

    intial commit for tech syndicate

---
+++ posts/forms.py
@@ -0,0 +1,11 @@
+from dataclasses import fields
+from django import forms
+from . models import Post
+from ckeditor.widgets import CKEditorWidget
+
+class PostForm(forms.ModelForm):
+    body = forms.CharField(widget=CKEditorWidget())
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+    class Meta:
+        model = Post
+        fields = ['image','title', 'body', 'draft', 'publish']
